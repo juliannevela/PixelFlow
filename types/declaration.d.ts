e5b0679510ecdd6a01e5256eccdcccf56e877cd0
Juli Vela <75726146+juliannevela@users.noreply.github.com>
2024-09-26 16:12:21 -0700

    added svgr for using SVGs directly

--- types/declaration.d.ts
+++ types/declaration.d.ts
@@ -2,3 +2,14 @@ declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module "*.svg" {
+  import { FC, SVGProps } from "react";
+  const content: FC<SVGProps<SVGElement>>;
+  export default content;
+}
+
+declare module "*.svg?url" {
+  const content: any;
+  export default content;
+}
